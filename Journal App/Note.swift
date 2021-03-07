eef00838740b7c21ee2cc95d04df4193cb5c2e07
Kyle Sherrington <kylejsherrington@gmail.com>
2021-03-07 13:14:09 -0800

    Initial Commit

---
+++ Journal App/Note.swift
@@ -0,0 +1,19 @@
+//
+//  Note.swift
+//  Journal App
+//
+//  Created by Kyle Sherrington on 2021-03-07.
+//
+
+import Foundation
+
+struct Note {
+    
+    var docId:String
+    var title:String
+    var body:String
+    var isStarred:Bool
+    var createdAt:Date
+    var lastUpdatedAt:Date
+    
+}
---
+++ Journal App/NotesModel.swift
@@ -0,0 +1,14 @@
+//
+//  NotesModel.swift
+//  Journal App
+//
+//  Created by Kyle Sherrington on 2021-03-07.
+//
+
+import Foundation
+
+class NotesModel {
+    
+    
+    
+}
